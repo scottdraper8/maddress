80d95dcc40e615fb0ed247fb2b5196bf50221726
Scott Draper <draper.tscott@gmail.com>
2021-07-20 13:20:41 -0600

    Edited setup fro alpha release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='maddress',                                # should match the package folder
     packages=['maddress'],                          # should match the package folder
-    version='1.0.0-beta1',                           # important for updates
+    version='1.0.0-alpha',                           # important for updates
     license='MIT',                                  # should match your chosen license
     description='Testing installation of Package',
     long_description=long_description,              # loads your README.md
@@ -28,5 +28,5 @@ setuptools.setup(
         'Programming Language :: Python :: 3.9',
     ],
     
-    download_url="https://github.com/scottdraper8/maddress/archive/refs/tags/v1.0.0-beta1.tar.gz",
+    download_url="https://github.com/scottdraper8/maddress/archive/refs/tags/v1.0.0-alpha.tar.gz",
 ) 
